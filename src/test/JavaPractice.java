bb4185c89575f27ffe0c9f388666e0d5854ac812
alekhya <alekhyametti2006@gmail.com>
2024-01-31 20:43:45 +0530

    @alex new project added

---
+++ src/test/JavaPractice.java
@@ -0,0 +1,13 @@
+package test;
+
+public class JavaPractice { // class starts here
+
+    static String studentName =  "vamshi";
+
+    public static void main(String args[])  //static method method
+    {
+       System.out.println("***** Student NAme is : "+studentName);
+    }
+
+
+}// class closes here
