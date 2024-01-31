ee17475afd1e4aae13f14393700df50858bf3894
alekhya <alekhyametti2006@gmail.com>
2024-01-31 21:09:08 +0530

    @alex sum function added

--- src/test/JavaPractice.java
+++ src/test/JavaPractice.java
@@ -1,12 +1,28 @@
 package test;
 
 public class JavaPractice { // class starts here
+    static String helloStatement = " hello";
+    static String studentName2=  "vamshi Ponnam";
+    static int sum;
 
-    static String studentName =  "vamshi";
+    public  static void helloStatementPrint(){
+        System.out.println("***** Student Name is : "+studentName2);
+        System.out.println("***** "+ helloStatement);
+    }
+
+    public static void addTwoNumbers(int a, int b) {
+        // code implementation
+        sum = a+b;
+        System.out.println("sum of two numbers :" + sum);
+    }
+
 
     public static void main(String args[])  //static method method
     {
-       System.out.println("***** Student NAme is : "+studentName);
+        helloStatementPrint();
+
+        addTwoNumbers(15,10);
+
     }
 
 
